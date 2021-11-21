e897dca620e287cd55851ff4958714279f143a54
nguyenpav <nguyenpav@gmail.com>
2021-11-22 00:38:14 +0700

    abc

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//route
+app.get('/', (req, res) => {
+return res.send('Hello World!')
+})
+//127.0.0.1  - localhost
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
